09893029c5d5ecef26c0365df26412733acbeaf8
Damely Tineo <damely.tineo07@gmail.com>
2019-12-09 18:53:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["blue", "pink", "yellow", "green"]
-  arr << "orange"
+  array << "orange"
 end
 
 def add_element_to_start_of_array(array, element)
