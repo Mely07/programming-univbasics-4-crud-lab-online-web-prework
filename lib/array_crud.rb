fcdc93e22cd7de0abc7785d7485ef30eb42b9107
Damely Tineo <damely.tineo07@gmail.com>
2019-12-09 18:38:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,12 @@
-def create_an_empty_array
-  
+def 
+  create_an_empty_array [ ]
 end
 
-def create_an_array
-  
+def 
+  create_an_array [ ]
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array(create_an_array, element)
   
 end
 
