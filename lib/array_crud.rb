83573dbcd065d9441d8d354a0436f3386d5330fb
Damely Tineo <damely.tineo07@gmail.com>
2019-12-09 18:43:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
-def 
+def create_an_empty_array
   create_an_empty_array [ ]
 end
 
-def 
-  create_an_array [ ]
+def create_an_array
+  
 end
 
-def add_element_to_end_of_array(create_an_array, << last item)
+def add_element_to_end_of_array(array, element)
   
-end;
+end
 
 def add_element_to_start_of_array(array, element)
   
-end;
+end
 
 def remove_element_from_end_of_array(array)
   
-end;
+end
 
 def remove_element_from_start_of_array(array)
   
-end;
+end
 
 def retrieve_element_from_index(array, index_number)
   
-end;
+end
 
 def retrieve_first_element_from_array(array)
   
-end;
+end
 
 def retrieve_last_element_from_array(array)
   
-end;
+end
 
 def update_element_from_index(array, index_number, element)
 
-end;
+end
